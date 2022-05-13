4b76174de14b2a0801d37be93762867b92264923
guilhermegouw <guilherme.gouw@gmail.com>
2022-05-13 11:41:21 -0300

    05/13/2022

--- bank_account/bank_oop/bank.py
+++ bank_account/bank_oop/bank.py
@@ -1,10 +1,33 @@
-from account import Account
+from account import AbortTransaction, Account
 
 
 class Bank:
-    def __init__(self):
+    def __init__(self, hours, address, phone):
         self.account_dict = {}
         self.next_account_number = 0
+        self.hours = hours
+        self.address = address
+        self.phone = phone
+
+    def ask_for_valid_account_number(self):
+        account_number = input('What is your account number? ')
+        try:
+            account_number = int(account_number)
+        except ValueError:
+            raise AbortTransaction('The account number must be an integer')
+        if account_number not in self.account_dict:
+            raise AbortTransaction(f'There is no account {account_number}')
+        return account_number
+
+    def ask_for_valid_password(self, account):
+        password = input('Please enter your password: ')
+        account.check_password_match(password)
+
+    def get_user_account(self):
+        account_number = self.ask_for_valid_account_number()
+        the_account = self.account_dict[account_number]
+        self.ask_for_valid_password(the_account)
+        return the_account
 
     def create_account(self, name, starting_amount, password):
         the_account = Account(name, starting_amount, password)
@@ -24,49 +47,42 @@ class Bank:
     
     def close_account(self):
         print('*** Close Account ***')
-        user_account_number = int(input('What is your account number? '))
-        user_password = input('What is your password? ')
-        the_account = self.account_dict[user_account_number]
-        the_balance = the_account.get_balance(user_password)
-        if the_balance is not None:
-            print(f'You had {the_balance} in your account, which is being returned to you.')
-            del self.account_dict[user_account_number]
-            print('Your account is closed.')
+        the_account = self.get_user_account()
+        the_balance = the_account.get_balance()
+        print(f'You had {the_balance} in your account, which is being returned to you.')
+        del the_account
+        print('Your account is closed.')
 
     def balance(self):
         print('*** Get Balance ***')
-        user_account_number = int(input('What is your account number? '))
-        user_password = input('What is your password? ')
-        the_account = self.account_dict[user_account_number]
-        the_balance = the_account.get_balance(user_password)
-        if the_balance is not None:
-            print(f'Your balance is: {the_balance}')
+        the_account = self.get_user_account()
+        the_balance = the_account.get_balance()
+        print(f'Your balance is: {the_balance}')
 
     def deposit(self):
         print('*** Deposit ***')
-        user_account_number = int(input('What is your account number? '))
-        deposit_amount = int(input('Please enter the amount to deposit: '))
-        user_password = input('What is your password? ')
-        the_account = self.account_dict[user_account_number]
-        the_balance = the_account.deposit(deposit_amount, user_password)
-        if the_balance is not None:
-            print(f'Your new balance is: {the_balance}')
+        the_account = self.get_user_account()
+        deposit_amount = input('Please enter the amount to deposit: ')
+        the_balance = the_account.deposit(deposit_amount)
+        print(f'Deposited: {deposit_amount}')
+        print(f'Your new balance is: {the_balance}')
 
     def withdraw(self):
         print('*** Withdraw ***')
-        user_account_number = int(input('What is your account number? '))
+        the_account = self.get_user_account()
         withdraw_amount = int(input('Please enter the amount to withdraw: '))
-        user_password = input('What is your password? ')
-        the_account = self.account_dict[user_account_number]
-        the_balance = the_account.withdraw(withdraw_amount, user_password)
-        if the_balance is not None:
-            print(f'Withdraw: {withdraw_amount}')
-            print(f'Your new balance is: {the_balance}')
+        the_balance = the_account.withdraw(withdraw_amount)
+        print(f'Withdraw: {withdraw_amount}')
+        print(f'Your new balance is: {the_balance}')
+
+    def get_info(self):
+        print(f'Hours: {self.hours}')
+        print(f'Address: {self.address}')
+        print(f'Phone: {self.phone}')
+        print(f'We currently have {len(self.account_dict)} account(s) open.')
 
     def show(self):
-        user_account_number = int(input('What is your account number? '))
-        user_password = input('What is your password? ')
-        the_account = self.account_dict[user_account_number]
-        the_account.show(user_password)
+        the_account = self.get_user_account()
+        the_account.show()
 
 
--- bank_account/bank_oop/menu.py
+++ bank_account/bank_oop/menu.py
@@ -1,13 +1,15 @@
+from account import AbortTransaction
 from bank import Bank
 
 
-the_bank = Bank()
+the_bank = Bank('9 to 5', '123 Main Street, AnyTown, USA', '(650) 555-1212')
 
 while True:
     print()
     print('Press b to get the balance')
     print('Press c to close an account')
     print('Press d to make a deposit')
+    print('Press i to get Bank information')
     print('Press o to open a new account')
     print('Press w to make a withdraw')
     print('Press s to show your account data')
@@ -18,20 +20,23 @@ while True:
     action = action[0]
     print()
 
-    if action == 'b':
-        the_bank.balance()
-    elif action == 'c':
-        the_bank.close_account()
-    elif action == 'd':
-        the_bank.deposit()
-    elif action == 'o':
-        the_bank.open_account()
-    elif action == 's':
-        the_bank.show()
-    elif action == 'w':
-        the_bank.withdraw()
-    elif action == 'q':
-        break
-    else:
-        print('Sorry, that was not a valid action. Please try again.')
+    try:
+        if action == 'b':
+            the_bank.balance()
+        elif action == 'c':
+            the_bank.close_account()
+        elif action == 'd':
+            the_bank.deposit()
+        elif action == 'i':
+            the_bank.get_info()
+        elif action == 'o':
+            the_bank.open_account()
+        elif action == 's':
+            the_bank.show()
+        elif action == 'w':
+            the_bank.withdraw()
+        elif action == 'q':
+            break
+    except AbortTransaction as error:
+        print(error)
 print('Done')
